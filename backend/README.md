1eab2db36905a39a25c6c83f582f159c001b7ba3
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-12 12:07:40 +0700

    Finish all thing

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+3 ห้อง 3 JSON
+
+```json
+{
+    "room_number": 1, // int
+    "status": false, // boolean, true = คนอยู่
+    "last_update": "2022-02-12 11:47:08.453951", // datetime
+    "total_time": 106.768687, // float
+    "total_session": 5 // int
+}
+```
--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,121 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from datetime import datetime
+from dateutil import parser
 
 app = FastAPI()
 
+client = MongoClient('mongodb://localhost', 27017)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+db = client["bathroom"]
+
+collection = db["room"]
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+class Bathroom(BaseModel):
+    room_number: int
+    status: bool
+    last_update: str
+    total_time: int
+    total_session: int
 
-@app.get("/test")
-async def test(name: str):
-    return {"message": f"Hello {name}"}
+current_time = str(datetime.now())
+
+# Reset all the data in the database to the default
+for i in range(1, 4):
+    # Find that is that room exist in database
+    result = collection.find_one({"room_number": i})
+    if result is None:
+        # If not, create a new one
+        collection.insert_one({
+            "room_number": i,
+            "status": False,
+            "last_update": current_time,
+            "total_time": 0,
+            "total_session": 0}
+        )
+    else:
+        # Update all value to default value
+        collection.update_one({"room_number": i}, {"$set": {
+            "room_number": i,
+            "status": False,
+            "last_update": current_time,
+            "total_time": 0,
+            "total_session": 0}}
+                              )
+
+
+# Return all the rooms
+@app.get("/bathroom/get")
+async def get_all_bathroom():
+    r = list(collection.find({}, {"_id": 0}))
+    if len(r) == 0:
+        return {"message": "No data"}
+    else:
+        return r
+
+
+# Return a specific room
+@app.get("/bathroom/get/{room_number}")
+async def get_bathroom(room_number: int):
+    r = collection.find_one({"room_number": room_number}, {"_id": 0})
+    return r
+
+
+# Return an average time for a specific room
+@app.get("/bathroom/get/average/{room_number}/")
+async def get_bathroom_average_time(room_number: int):
+    r = collection.find_one({"room_number": room_number}, {"_id": 0})
+    return {"average_time": r["total_time"] / r["total_session"]}
+
+
+# Return ad average time for all the rooms
+@app.get("/bathroom/get/average/all")
+async def get_all_bathroom_average_time():
+    total_time_all_room = 0
+    total_session_all_room = 0
+    r = list(collection.find({}, {"_id": 0}))
+    for i in r:
+        total_time_all_room += i["total_time"]
+        total_session_all_room += i["total_session"]
+    if total_session_all_room == 0:
+        return {"average_time": 0}
+    return {"average_time": total_time_all_room / total_session_all_room}
+
+
+# Put a new value to the target bathroom
+@app.put("/bathroom/change/{room_number}/{status}")
+async def change_bathroom_status(room_number: int, status: bool):
+    r = collection.find_one({"room_number": room_number}, {"_id": 0})
+    if r is None:
+        return HTTPException(status_code=404, detail="Room not found")
+    else:
+        # Check if the status is not same with database for safety
+        if r["status"] != status:
+            # It can update the status
+            # First case, just get in
+            if r["status"] is False and status is True:
+                # Update the status and last update time
+                new_status = {
+                    "status": status,
+                    "last_update": str(datetime.now())
+                }
+                collection.update_one({"room_number": room_number}, {"$set": new_status})
+            else:
+                # Second case, just get out
+                # Update the status, total
+                # Calculate the total time in second
+                total_time = r["total_time"] + (datetime.now() - parser.parse(r["last_update"])).total_seconds()
+                new_status = {
+                    "status": status,  # Update the status
+                    "total_time": total_time,  # Update the total time
+                    "last_update": str(datetime.now()),  # Update the last update time
+                    "total_session": r["total_session"] + 1  # Update the total session
+                }
+                # Update the total time
+                collection.update_one({"room_number": room_number}, {"$set": new_status})
+        else:
+            raise HTTPException(status_code=400, detail="Status is same with database")
+    return {"message": "Success"}
